d8299190e2a8ed2adedb026f709dfc809f06fcfe
kirank22 <kirankothapalli22@gmail.com>
2025-08-08 15:35:15 -0400

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Kiran Kothapalli on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
